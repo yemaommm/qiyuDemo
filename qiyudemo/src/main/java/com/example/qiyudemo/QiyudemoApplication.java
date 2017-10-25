7f3f73e3b85ffec98cb2ea636216d4ffa5fcfcf9
weichen.tang <46729977@qq.com>
2017-10-25 13:58:00 +0800

    init

---
+++ qiyudemo/src/main/java/com/example/qiyudemo/QiyudemoApplication.java
@@ -0,0 +1,12 @@
+package com.example.qiyudemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QiyudemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QiyudemoApplication.class, args);
+	}
+}
